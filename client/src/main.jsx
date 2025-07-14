b7f53e23e942721a99f29ba4ac28161ff2638d78
soumen-github-account <soumendas7584@gmail.com>
2025-07-14 20:45:08 +0530

    app

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -4,7 +4,7 @@ import './index.css'
 import App from './App.jsx'
 import { BrowserRouter } from 'react-router-dom'
 import { ClerkProvider } from '@clerk/clerk-react'
-import AppContextProvider from './contexts/appContext.jsx'
+import AppContextProvider from './contexts/AppContext.jsx'
 
 // Import your Publishable Key
 // const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY
