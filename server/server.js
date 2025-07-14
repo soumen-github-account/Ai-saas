7075b728640cd194a9dfd29d692f67806f961978
soumen-github-account <soumendas7584@gmail.com>
2025-07-14 20:58:18 +0530

    server

--- server/server.js
+++ server/server.js
@@ -15,28 +15,29 @@ const app = express()
 app.use(express.json())
 app.use(cookieParser())
 
-app.use(cors({
-  origin: 'http://localhost:5173',  // your React app's URL
-  credentials: true
-}));
+// app.use(cors({
+//   origin: 'http://localhost:5173',  // your React app's URL
+//   credentials: true
+// }));
 
 connectDB();
 connectCloudinary();
 // app.use(clerkMiddleware())
-// const allowedOrigins = [
-//   'http://localhost:5173'
-// ];
+const allowedOrigins = [
+  'http://localhost:5173',
+  'https://ai-saas-frontend-nk3k.onrender.com'
+];
 
-// app.use(cors({
-//   origin: function (origin, callback) {
-//     if (!origin || allowedOrigins.includes(origin)) {
-//       callback(null, true);
-//     } else {
-//       callback(new Error('Not allowed by CORS'));
-//     }
-//   },
-//   credentials: true
-// }));
+app.use(cors({
+  origin: function (origin, callback) {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
+  credentials: true
+}));
 
 app.get('/', (req,res)=>res.send('Server is live'))
 
