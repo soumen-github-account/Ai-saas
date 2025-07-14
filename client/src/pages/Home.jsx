c96501c6627f09068edb3ea64346741ada51b389
soumen-github-account <soumendas7584@gmail.com>
2025-07-14 20:48:35 +0530

    home

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import Navbar from '../components/Navbar'
-import Hero from '../components/hero'
+import Hero from '../components/Hero'
 import Aitools from '../components/Aitools'
 import Testimonial from '../components/Testimoial'
 import Plan from '../components/Plan'
